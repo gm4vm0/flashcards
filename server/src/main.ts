da6616c31f2dcbf96e64eff5ee4cc46823c36db5
sammi <sshih5678@gmail.com>
2023-07-28 15:46:28 +0800

    fix(server/src/main): enable CORS with credentials

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
