9c907cfd5f475bd02c9f3141b45980a47573fd70
sammi <sshih5678@gmail.com>
2023-08-13 22:29:58 +0800

    feat(client/src/routes/Index.tsx): make flashcards view protected

--- client/src/routes/Index.tsx
+++ client/src/routes/Index.tsx
@@ -1,8 +1,13 @@
 import { Center } from "@mantine/core";
 import AddFlashcardModal from "@/features/flashcards/add-flashcard/AddFlashcardModal";
 import Flashcards from "@/features/flashcards/Flashcards";
+import useAuthStore from "@/stores/auth-store";
 
 function Index() {
+  const user = useAuthStore((state) => state.user);
+
+  if (!user) return <p>Log in first</p>;
+
   return (
     <Center w="100%" h="100%" sx={{ flexDirection: "column", padding: 0 }}>
       <AddFlashcardModal />
