7dee944733ad72a4ccfde8900f2e5e5596110ae7
sammi <sshih5678@gmail.com>
2023-08-14 22:03:27 +0800

    feat(server): implement auth for cards

--- server/src/auth/types/authenticated-request.type.ts
+++ server/src/auth/types/authenticated-request.type.ts
@@ -1,4 +1,6 @@
-import { User } from '@prisma/client';
 import { Request } from 'express';
+import { UserWithDecks } from './user-with-decks.type';
 
-export type AuthenticatedRequest = Omit<Request, 'user'> & { user: User };
+export type AuthenticatedRequest = Omit<Request, 'user'> & {
+  user: UserWithDecks;
+};
--- server/src/cards/dto/create-card.dto.ts
+++ server/src/cards/dto/create-card.dto.ts
@@ -8,7 +8,4 @@ export class CreateCardDto {
   @IsNotEmpty()
   @MaxLength(30)
   back: string;
-
-  @IsNotEmpty()
-  deckId: string;
 }
